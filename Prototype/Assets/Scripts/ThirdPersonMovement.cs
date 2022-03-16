0f6bed8beff3ecc8b06e842e7fcc17ce3c028801
Amy Paulsen <apaulsen@student.pps.net>
2022-03-16 11:08:52 -0700

    Jumping
    Started to work on jumping.

--- Prototype/Assets/Scripts/ThirdPersonMovement.cs
+++ Prototype/Assets/Scripts/ThirdPersonMovement.cs
@@ -2,34 +2,32 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class ThirdPersonMovement : MonoBehaviour
-{
+public class ThirdPersonMovement : MonoBehaviour {
     public CharacterController controller;
     public Transform cam;
 
     public float speed = 6f;
-
+    public float jumpHeight = 1f;
+    //Turning/looking:
     public float turnSmoothTime = 0.1f;
     float tunrSmoothVelocity;
-    // Start is called before the first frame update
-    void Start() {
-        
-    }
 
-    // Update is called once per frame
     void Update() {
+        //Player WASD movement:
         float horizontal = Input.GetAxisRaw("Horizontal");
         float vertical = Input.GetAxisRaw("Vertical");
         Vector3 direction = new Vector3(horizontal, 0f, vertical).normalized;
 
         if(direction.magnitude >= 0.1f) {
-
             float targetAngle = Mathf.Atan2(direction.x, direction.z) * Mathf.Rad2Deg + cam.eulerAngles.y;
             float angle = Mathf.SmoothDampAngle(transform.eulerAngles.y, targetAngle, ref tunrSmoothVelocity, turnSmoothTime);
             transform.rotation = Quaternion.Euler(0f, angle, 0f);
 
             Vector3 moveDir = Quaternion.Euler(0f, targetAngle, 0f) * Vector3.forward;
             controller.Move(moveDir.normalized * speed * Time.deltaTime);
+        }
+        if (Input.GetKeyDown("space")) {
+            transform.position = transform.position + new Vector3(0f, 1f, 0f).normalized;
         }
     }
 }
