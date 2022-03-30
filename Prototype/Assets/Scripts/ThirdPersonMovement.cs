18bad4869146c70f9f90aa0ea4e39010b5b29f78
Amy Paulsen <apaulsen@student.pps.net>
2022-03-30 11:12:38 -0700

    More Jumping
    More work on jumping. almost there.

--- Prototype/Assets/Scripts/ThirdPersonMovement.cs
+++ Prototype/Assets/Scripts/ThirdPersonMovement.cs
@@ -3,20 +3,26 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class ThirdPersonMovement : MonoBehaviour {
+    //moving junk
     public CharacterController controller;
     public Transform cam;
-
     public float speed = 6f;
-    //Turning/looking:
-    public float turnSmoothTime = 0.1f;
+    //Jumping junk
+    private Vector3 playerVelocity;
+    public bool groundedPlayer;
+    public float jumpHeight = 2.0f;
+    public float gravityValue = -9.81f;
+    
+    //Rotating/looking:
+    private float turnSmoothTime = 0.1f;
     float tunrSmoothVelocity;
 
     void Update() {
-        //Player WASD movement:
+        //WASD movement:
         float horizontal = Input.GetAxisRaw("Horizontal");
         float vertical = Input.GetAxisRaw("Vertical");
         Vector3 direction = new Vector3(horizontal, 0f, vertical).normalized;
-
+        
         if(direction.magnitude >= 0.1f) {
             float targetAngle = Mathf.Atan2(direction.x, direction.z) * Mathf.Rad2Deg + cam.eulerAngles.y;
             float angle = Mathf.SmoothDampAngle(transform.eulerAngles.y, targetAngle, ref tunrSmoothVelocity, turnSmoothTime);
@@ -24,52 +30,19 @@ public class ThirdPersonMovement : MonoBehaviour {
 
             Vector3 moveDir = Quaternion.Euler(0f, targetAngle, 0f) * Vector3.forward;
             controller.Move(moveDir.normalized * speed * Time.deltaTime);
-        }
+        }  
+        groundedPlayer = controller.isGrounded;
+    }
+
+    void FixedUpdate() {
         //Jumping
-        //if (Input.GetKeyDown(KeyCode.Space)) {
-            //transform.position = transform.position + new Vector3(0f, jumpHeight, 0f).normalized;
-        //}
-        
-        ////
-        if (Input.GetAxisRaw("Jump")) {
-                Debug.Log("Input inputs");
-                velocity = Mathf.Sqrt(jumpHeight * -2f * (gravityScale));
-            }
-            velocity += gravity * gravityScale * Time.deltaTime;
-            MovePlayer();
-    }
-
-
-        public CharacterController cc;
-        public float gravity = -9.81f;
-        public float gravityScale = 1;
-        public float jumpHeight = 4;
-        float velocity;
-        
-        void MovePlayer() {
-            cc.Move(new Vector3(0, velocity, 0) * Time.deltaTime);
+        if (Input.GetButtonDown("Jump") && groundedPlayer) {
+            playerVelocity.y += Mathf.Sqrt(jumpHeight * -3.0f * gravityValue);
+            
+            Debug.Log("jumps");
         }
-    
-
-/*public class CharacterControllerJump : MonoBehaviour {
-    public CharacterController cc;
-    public float gravity = -9.81f;
-    public float gravityScale = 1;
-    public float jumpHeight = 4;
-    float velocity;
-
-    void Update() {
-        if (Input.GetKeyDown(KeyCode.Space)) {
-            Debug.Log("Input inputs");
-            velocity = Mathf.Sqrt(jumpHeight * -2f * (gravityScale));
-        }
-        velocity += gravity * gravityScale * Time.deltaTime;
-        MovePlayer();
+        playerVelocity.y += gravityValue * Time.deltaTime;
+        controller.Move(playerVelocity * Time.deltaTime);
+        
     }
-    void MovePlayer() {
-        cc.Move(new Vector3(0, velocity, 0) * Time.deltaTime);
-    }
-}*/
 }
-
-        
